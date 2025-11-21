fae3630f05f8447b120ea89f95dc42f4aaef16fd
sn0rkmaiden <alisapetrova275@gmail.com>
2025-11-21 22:51:29 +0300

    modified saved path to include evaluation sets

--- l2l.py
+++ l2l.py
@@ -134,6 +134,8 @@ if __name__ == "__main__":
     else:  
         # Single file: "5"  
         evaluation_set = [int(args.evaluation_set)]
+
+    eval_tag = f"set{args.evaluation_set.replace(',', '_').replace('-', '_')}"
     
     player_chat_mode = args.player_chat_mode
     task_data_path = args.task_data_path
@@ -173,7 +175,7 @@ if __name__ == "__main__":
         output_path = "results/l2l_llama3.1-405B.{}.{}.json".format(mode,language)
     elif args.seeker_agent_llm == 'deepseek':
         player_llm = CustomLLM(args.seeker_agent_llm, api_key=hftoken, cache=f'log/llm_player_cache_deepseek.pkl')
-        output_path = "results/l2l_deepseek.{}.{}.json".format(mode,language)
+        output_path = "results/l2l_deepseek.{}.{}.{}.json".format(mode,language, eval_tag)
     elif args.seeker_agent_llm == 'gemma':
         player_llm = HookedGEMMA(
             model_name="gemma-2b-it",
@@ -196,11 +198,11 @@ if __name__ == "__main__":
         else:
             suffix = "nosteering"
 
-        output_path = "results/l2l_gemma.{}.{}.{}.json".format(mode, language, suffix)
+        output_path = "results/l2l_gemma.{}.{}.{}.{}.json".format(mode, language, eval_tag, suffix)
 
     else:
         player_llm = HuggingFaceLLM(args.seeker_agent_llm, f'log/{args.seeker_agent_llm.split("/")[-1]}_plyaer_cache.pkl')
-        output_path = "results/l2l_{}.{}.{}.json".format(args.seeker_agent_llm.split("/")[-1], mode, language)
+        output_path = "results/l2l_{}.{}.{}.{}.json".format(args.seeker_agent_llm.split("/")[-1], mode, language, eval_tag)
 
     evaluate_player(task_data_path, output_path, player_llm, player_chat_mode, provider_agent_constructor, args.provider_agent_llm, hftoken, evaluation_set)
 
